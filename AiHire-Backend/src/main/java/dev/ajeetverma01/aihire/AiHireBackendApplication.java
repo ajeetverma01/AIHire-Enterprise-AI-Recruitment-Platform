b33af9838bf5907cc6c795678697c19683736cf4
Ajeet Verma <av401402@gmail.com>
2026-09-06 12:02:07 +0530

    initial project setup

---
+++ AiHire-Backend/src/main/java/dev/ajeetverma01/aihire/AiHireBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.ajeetverma01.aihire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiHireBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiHireBackendApplication.class, args);
+	}
+
+}
---
+++ AiHire-Backend/src/test/java/dev/ajeetverma01/aihire/AiHireBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ajeetverma01.aihire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiHireBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
