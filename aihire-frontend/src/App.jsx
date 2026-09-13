9f140962c7b774de85f921a2fba9ee34d5c59106
Ajeet Verma <av401402@gmail.com>
2026-09-13 10:48:58 +0530

    basic frontend setup

--- aihire-frontend/src/App.jsx
+++ aihire-frontend/src/App.jsx
@@ -1,12 +1,42 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+import RecruiterDashboard from "./pages/recruiter/RecruiterDashboard";
+import CandidateDashboard from "./pages/candidate/CandidateDashboard";
+import AdminDashboard from "./pages/admin/AdminDashboard";
 
 function App() {
-
   return (
-    <>
-      <h1>Hello</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/recruiter/dashboard"
+          element={<RecruiterDashboard />}
+        />
+
+        <Route
+          path="/candidate/dashboard"
+          element={<CandidateDashboard />}
+        />
+
+        <Route
+          path="/admin/dashboard"
+          element={<AdminDashboard />}
+        />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
